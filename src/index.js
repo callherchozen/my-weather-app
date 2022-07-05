e8adb7f9cd6006cbd6fe6add7e71d960da06234e
callherchozen <108554299+callherchozen@users.noreply.github.com>
2022-07-05 07:19:26 -0500

    Fixed cookie specification

--- src/index.js
+++ src/index.js
@@ -102,7 +102,6 @@ function displayTemperature(response) {
 function search(city) {
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  const axios = require("axios").default;
   axios.get(apiUrl).then(displayTemperature);
 }
 
